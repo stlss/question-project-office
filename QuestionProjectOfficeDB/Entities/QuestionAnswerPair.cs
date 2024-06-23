5a6bbff8740bf00f29093430392a10eebf4f03c2
IvakinAD <adivakin@edu.hse.ru>
2024-06-23 13:53:41 +0500

    feat: Добавлена библиотека классов под EF.

---
+++ QuestionProjectOfficeDB/Entities/QuestionAnswerPair.cs
@@ -0,0 +1,17 @@
+﻿namespace QuestionProjectOfficeDb.Entities
+{
+    public class QuestionAnswerPair
+    {
+        public Guid Id { get; set; }
+
+        public string Question { get; set; } = null!;
+
+        public string Answer { get; set; } = null!;
+
+        public QuestionCategory? Category { get; set; }
+
+        public int Populatiry { get; set; } = 0;
+
+        public DateTime DateTime { get; set; }
+    }
+}
---
+++ QuestionProjectOfficeDB/QuestionProjectOfficeContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuestionProjectOfficeDb.Entities;
+
+namespace QuestionProjectOfficeDb
+{
+    public class QuestionProjectOfficeContext(DbContextOptions<QuestionProjectOfficeContext> options) : DbContext(options)
+    {
+        public DbSet<QuestionAnswerPair> QuestionAnswerPairs { get; set; }
+
+        public DbSet<QuestionCategory> QuestionCategories { get; set; }
+
+
+        public static DbContextOptions<QuestionProjectOfficeContext> BuildDbContextOptions(string connectionString, ProviderDb providerDb)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<QuestionProjectOfficeContext>();
+
+            switch (providerDb)
+            {
+                case ProviderDb.PostgreSql:
+                    optionsBuilder.UseNpgsql(connectionString);
+                    break;
+            }
+
+            return optionsBuilder.Options;
+        }
+    }
+}
