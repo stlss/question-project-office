ab88b97699698cb0f2ff5ef703945f7f3edcd249
IvakinAD <adivakin@edu.hse.ru>
2024-06-23 13:25:59 +0500

    feat: Добавлен шаблон MVVM.

---
+++ QuestionProjectOffice/ViewModels/Windows/MainWindowViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace QuestionProjectOffice.ViewModels.Windows
+{
+    internal class MainWindowViewModel : ObservableObject
+    {
+
+    }
+}
---
+++ QuestionProjectOffice/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace QuestionProjectOffice.Views.Windows
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
