0b248f2b79fa3e79951977f4478ed832d86ae72c
IvakinAD <adivakin@edu.hse.ru>
2024-06-23 14:29:39 +0500

    feat: Добавлено создание контекста базы данных в основном приложении.

---
+++ QuestionProjectOffice/Models/ConnectionQuestionProjectOfficeDb.cs
@@ -0,0 +1,7 @@
+﻿namespace QuestionProjectOffice.Models
+{
+    internal static class ConnectionQuestionProjectOfficeDb
+    {
+        public static string PostgreSqlConnectionString => "Host=localhost;Port=5432;Database=QuestionProjectOffice;Username=postgres;Password=12345";
+    }
+}
--- QuestionProjectOffice/ViewModels/Windows/MainWindowViewModel.cs
+++ QuestionProjectOffice/ViewModels/Windows/MainWindowViewModel.cs
@@ -1,9 +1,47 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using QuestionProjectOffice.Models;
+using QuestionProjectOfficeDb;
 
 namespace QuestionProjectOffice.ViewModels.Windows
 {
     internal class MainWindowViewModel : ObservableObject
     {
+        #region Поля
+        private readonly QuestionProjectOfficeContext _dbContext;
+        #endregion
 
+
+        public MainWindowViewModel()
+        {
+            _dbContext = CreateDbContext();
+        }
+
+
+        #region Методы
+
+        #region Создание контекста базы данных и её заполнение
+        private static QuestionProjectOfficeContext CreateDbContext()
+        {
+            var connectionString = ConnectionQuestionProjectOfficeDb.PostgreSqlConnectionString;
+            var providerDb = ProviderDb.PostgreSql;
+
+            var options = QuestionProjectOfficeContext.BuildDbContextOptions(connectionString, providerDb);
+
+            var _dbContext = new QuestionProjectOfficeContext(options);
+            var isDatabaseCreated = _dbContext.Database.EnsureCreated();
+
+            if (isDatabaseCreated)
+                FillDataBase();
+
+            return _dbContext;
+        }
+
+        private static void FillDataBase()
+        {
+
+        }
+        #endregion
+
+        #endregion
     }
 }
