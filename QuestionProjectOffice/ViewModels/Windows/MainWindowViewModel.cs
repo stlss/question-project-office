27168b4075e68662e8a63d2b2a5b824656419623
IvakinAD <adivakin@edu.hse.ru>
2024-06-25 17:06:10 +0500

    update: Доработан макет главного окна.

--- QuestionProjectOffice/ViewModels/Windows/MainWindowViewModel.cs
+++ QuestionProjectOffice/ViewModels/Windows/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using QuestionProjectOffice.Models;
 using QuestionProjectOfficeDb;
 using QuestionProjectOfficeDb.Entities;
@@ -12,6 +13,124 @@ namespace QuestionProjectOffice.ViewModels.Windows
         #endregion
 
 
+        #region Свойства
+
+        #region Категории вопросов
+        // Отображаемые категории вопросов
+        public List<QuestionCategory> QuestionCategories { get; private set; }
+
+        // Выбранная категория вопросов пользователем
+        private QuestionCategory? _selectedQuestionCategory;
+        public QuestionCategory? SelectedQuestionCategory
+        {
+            get => _selectedQuestionCategory;
+            set
+            {
+                if (!SetProperty(ref _selectedQuestionCategory, value))
+                    return;
+
+                UpdateQuestionCategoryCommand.NotifyCanExecuteChanged();
+                DeleteQuestionCategoryCommand.NotifyCanExecuteChanged();
+            }
+        }
+        #endregion
+
+        #endregion
+
+
+        #region Команды
+
+        #region Добавление, изменение, удаление категорий вопросов
+
+        #region Добавить категорию
+        public RelayCommand CreateQuestionCategoryCommand { get; private set; }
+
+        private void ExecuteCreateQuestionCategoryCommand()
+        {
+
+        }
+        #endregion
+
+        #region Изменить категорию
+        public RelayCommand UpdateQuestionCategoryCommand { get; private set; }
+
+        private void ExecuteUpdateQuestionCategoryCommand()
+        {
+
+        }
+
+        private bool CanExecuteUpdateQuestionCategoryCommand() => _selectedQuestionCategory != null;
+        #endregion
+
+        #region Удалить категорию
+        public RelayCommand DeleteQuestionCategoryCommand { get; private set; }
+
+        private void ExecuteDeleteQuestionCategoryCommand()
+        {
+
+        }
+
+        private bool CanExecuteDeleteQuestionCategoryCommand() => _selectedQuestionCategory != null;
+        #endregion
+
+        #endregion
+
+        #region Выбор других категорий
+
+        #region Выбрать все вопросы
+        public RelayCommand SelectAllQuestionsCommand { get; private set; }
+
+        private void ExecuteSelectAllQuestionsCommand()
+        {
+            SelectedQuestionCategory = null;
+        }
+        #endregion
+
+        #region Выбрать вопросы без категорий
+        public RelayCommand SelectQuestionsWithoutCategoriesCommand { get; private set; }
+
+        private void ExecuteSelectQuestionsWithoutCategoriesCommand()
+        {
+            SelectedQuestionCategory = null;
+        }
+        #endregion
+
+        #endregion
+
+        #region Добавление, изменение, удаление вопросов и ответов
+
+        #region Добавить вопрос
+        public RelayCommand CreateQuestionCommand { get; private set; }
+
+        private void ExecuteCreateQuestionCommand()
+        {
+
+        }
+        #endregion
+
+        #region Изменить вопрос
+        public RelayCommand UpdateQuestionCommand { get; private set; }
+
+        private void ExecuteUpdateQuestionCommand()
+        {
+
+        }
+        #endregion
+
+        #region Удалить вопрос
+        public RelayCommand DeleteQuestionCommand { get; private set; }
+
+        private void ExecuteDeleteQuestionCommand()
+        {
+
+        }
+        #endregion
+
+        #endregion
+
+        #endregion
+
+
         public MainWindowViewModel()
         {
             #region Создания контекста базы данных и заполнения её.
@@ -21,6 +140,21 @@ namespace QuestionProjectOffice.ViewModels.Windows
             if (isCreatedDataBase && isFillDataBase)
                 FillDataBase();
             #endregion
+
+            #region Команды
+            CreateQuestionCategoryCommand = new(ExecuteCreateQuestionCategoryCommand);
+            UpdateQuestionCategoryCommand = new(ExecuteUpdateQuestionCategoryCommand, CanExecuteUpdateQuestionCategoryCommand);
+            DeleteQuestionCategoryCommand = new(ExecuteDeleteQuestionCategoryCommand, CanExecuteDeleteQuestionCategoryCommand);
+
+            SelectAllQuestionsCommand = new(ExecuteSelectAllQuestionsCommand);
+            SelectQuestionsWithoutCategoriesCommand = new(ExecuteSelectQuestionsWithoutCategoriesCommand);
+
+            CreateQuestionCommand = new(ExecuteCreateQuestionCommand);
+            UpdateQuestionCommand = new(ExecuteUpdateQuestionCommand);
+            DeleteQuestionCommand = new(ExecuteDeleteQuestionCommand);
+            #endregion
+
+            QuestionCategories = _dbContext.QuestionCategories.OrderBy(c => c.Name).ToList();
         }
 
 
